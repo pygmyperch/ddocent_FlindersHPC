6b0b87372f6909dc43d7d443c648d95cc7999906
Chris Brauer <pygmyperch@gmail.com>
2026-04-10 12:54:59 +0930

    Update .gitignore, remove dev files from tracking

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Deploy scripts to HPC scratch.
-# Usage: ./deploy.sh
-# Add --dry-run to preview without transferring.
-
-REMOTE="brau0037@deepthought.flinders.edu.au:/scratch/user/brau0037/kelp/ddocent/scripts/"
-
-rsync -avz --progress "$@" scripts/ "$REMOTE"
